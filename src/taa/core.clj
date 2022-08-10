58e2faaae843fcec532ddaa7cf56332e15504aef
craig <craig.flewelling@gmail.com>
2022-08-10 19:25:10 +0000

    Added dumb taa.core file.

---
+++ src/taa/core.clj
@@ -0,0 +1,4 @@
+(ns taa.core
+  ;;Require both capacity and requirements here so that the user can
+  ;;call functions in either from this init namespace.
+  (:require [taa [capacity :as capacity]]))
