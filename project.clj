0e64e8310579a2714231c2db5591c2ba42f64f39
tom <tom@the-cloud>
2023-01-11 18:37:34 +0000

    bumped version in project

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject taa "0.0.3-SNAPSHOT"
+(defproject taa "0.0.4-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [marathon "4.2.5-SNAPSHOT"]
