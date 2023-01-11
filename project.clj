5c47253de170fb13ca20943b142e7f0b594ca869
tom <tom@the-cloud>
2023-01-11 18:11:52 +0000

    Bumped release.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject taa "0.0.2-SNAPSHOT"
+(defproject taa "0.0.3-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [marathon "4.2.5-SNAPSHOT"]
