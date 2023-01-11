4ab4388994337ea8dc494131965da9aed068a333
tom <tom@the-cloud>
2023-01-11 17:28:54 +0000

    Integration with lein-git-down

--- project.clj
+++ project.clj
@@ -1,16 +1,7 @@
 (defproject taa "0.0.2-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 ;;Exclude tools.reader.  Else, an error occurs.
-                 [spork "0.2.1.6-SNAPSHOT" :exclusions
-                  [org.clojure/tools.reader
-                   ;;need to exclude for tech.ml.dataset
-                   ;;com.taoensso/nippy                   
-                   ]]
-                 [demand_builder "0.1.1-SNAPSHOT"]
-                 ;;currently, marathon is not pushed to clojars to
-                 ;;need to lein install from the marathon repo first
-                 [marathon "4.2.4-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [marathon "4.2.5-SNAPSHOT"]
                  ;;not in clojars either, this is fs-c's fork.
                  [smiletest "0.1.0-SNAPSHOT"]
                  ;[com.clojure-goes-fast/clj-memory-meter "0.2.1"]
@@ -28,16 +19,9 @@
              :dev {:resource-paths ["test/resources"]
                                         :jvm-opts ^:replace ["-Xmx8g"]
                    :source-paths ["../marathon/src"]
-                   }
-             :uberjar {:aot [taa.main]
-                       :main  taa.main
-                       :jvm-opts ^:replace ["-Xmx1000m" "-XX:NewSize=200m" "-server"]
-                       :plugins [[lein-capsule "0.2.1"]]
-                       ;;put the test dir in here so we can run tests from the capsule
-                       :source-paths ["src" "test"]
-                       :resource-paths ["test/resources"]
-                       }}
-  ;;default is thin.  This should include all dependencies in the capsule.
-  :capsule {:types {:fat {}}
-             :execution {:runtime {:jvm-args ["-Xmx8g"]}} }
-)
+                   }}
+  :plugins [[reifyhealth/lein-git-down "0.4.1"]]
+  :middleware [lein-git-down.plugin/inject-properties]
+  :repositories [["public-github" {:url "git://github.com"}]]
+  :git-down {marathon {:coordinates fsdonks/m4}}
+  )
