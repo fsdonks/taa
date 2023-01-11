247b1dba7e8cc2d2312105ae4ff3b7b8343a3c28
tom <tom@the-cloud>
2023-01-11 17:59:34 +0000

    Initial working version, tests pass.

--- project.clj
+++ project.clj
@@ -7,7 +7,8 @@
                  ;[com.clojure-goes-fast/clj-memory-meter "0.2.1"]
                  ;[techascent/tech.ml.dataset "7.000-beta-2"]
                  ]
-  :repl-options {:init-ns taa.core}
+  :repl-options {:init-ns taa.core
+                 :timeout 120000}
   ;;This stuff below will allow us to build a runnable jar that
   ;;includes all dependencies by calling
   ;;lein with-profile uberjar capsule
@@ -23,5 +24,5 @@
   :plugins [[reifyhealth/lein-git-down "0.4.1"]]
   :middleware [lein-git-down.plugin/inject-properties]
   :repositories [["public-github" {:url "git://github.com"}]]
-  :git-down {marathon {:coordinates fsdonks/m4}}
-  )
+  :git-down {marathon  {:coordinates  fsdonks/m4}
+             smiletest {:coordinates  fs-tom/krigingdemo}})
