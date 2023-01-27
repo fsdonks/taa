c323c81f49960aaa5dbe70013e1bb847bfdb0452
craig <craig.flewelling@gmail.com>
2023-01-27 15:57:02 +0000

    Added test for computing peak demand by period from a workbook.

--- test/taa/demandanalysis_test.clj
+++ test/taa/demandanalysis_test.clj
@@ -10,5 +10,37 @@
                   ])
 (def input-resources (req-test/as-resources input-paths))
 
-(proclysis/peaks-from (first input-resources))
-(proclysis/peaks-from (second input-resources))
+(def verified-peaks1
+  (set
+   [{:peak 1.0,
+    :intervals [[1.0 260.0]],
+    :group "All",
+    :period "PreSurge"}
+   {:peak 0, :intervals [], :group "All", :period "Surge"}
+   {:peak 1.0,
+    :intervals [[1000.0 1259.0]],
+    :group "All",
+    :period "PostSurge"}]))
+
+(def verified-peaks2
+  (set
+   [{:peak 895.0,
+    :intervals [[91.0 363.0]],
+    :group "All",
+    :period "PreSurge"}
+   {:peak 2387.0,
+    :intervals [[822.0 1022.0]],
+    :group "All",
+    :period "Surge"}
+   {:peak 0, :intervals [], :group "All", :period "PostSurge"}]))
+
+;;This is really a proc test, but I was just checking behavior with
+;;inputs in this repo.
+(deftest checking-peaks
+  (let
+      [[peaks-1 peaks-2] (map proclysis/peaks-from input-resources)]
+    (is (= (set peaks-1) verified-peaks1)
+        "Check if peaks-from is working properly on this simple
+case.")
+    (is (= (set peaks-2) verified-peaks2)
+        "Check if peaks-from is working properly on a more complicated case.")))
