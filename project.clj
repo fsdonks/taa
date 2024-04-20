230950a436ef0b563b93a6c2a36abff02f1ce1db
tom <tom@that-place>
2024-04-20 08:38:17 -0500

    Added exceptions to incomplete patches.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject taa "0.0.19-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [marathon "4.2.15-SNAPSHOT"]
+                 [marathon "d2eb63486ccd90ca0cd9c3875e896aa0007db785" #_"4.2.15-SNAPSHOT"]
                  ;[com.clojure-goes-fast/clj-memory-meter "0.2.1"]
                  ;[techascent/tech.ml.dataset "7.000-beta-2"]
                  ]
@@ -14,7 +14,7 @@
   :profiles {;;load our tests from resources just like in the uberjar
              :dev {:resource-paths ["test/resources"]
                    :jvm-opts ^:replace ["-Xmx8g"]
-                   :source-paths []
+                   :source-paths ["../marathon/src"]
                    }}
   :plugins [[reifyhealth/lein-git-down "0.4.1"]]
   :middleware [lein-git-down.plugin/inject-properties]
--- src/taa/core.clj
+++ src/taa/core.clj
@@ -3,7 +3,20 @@
   ;;call functions in either from this init namespace.
   (:require [taa [capacity :as capacity]
              [requirements :as requirements]
-             [demandanalysis :as analysis]]))
+             [demandanalysis :as analysis]]
+            [marathon.analysis.random :as random]))
+
+;;big-srcs undefined in monkey patch.
+;;inputs-outputs-path was undefined in the monkey patch.
+(println [:WARNING '[taa.core inputs-outputs-path big-srcs computer-name] :STUBBED-WITH-PLACEHOLDERS])
+(def inputs-outputs-path "")
+(def big-srcs #{})
+(def computer-name "")
+
+;;temporary error to prevent usage until we get this sorted out.
+(defn placeholder-error []
+  (throw (ex-info "fix placeholders!"
+            {:in [:WARNING '[taa.core inputs-outputs-path big-srcs computer-name] :STUBBED-WITH-PLACEHOLDERS]})))
 
 (defn m4-path [demand-name]
   (str inputs-outputs-path "m4_book_" demand-name ".xlsx"))
@@ -17,6 +30,7 @@
          :include-no-demand false))
 
 (defn rc-runs [input-map filter-big? comp-name demand-name i threads]
+  (placeholder-error)
   (capacity/do-taa-runs (m4-path demand-name)
     (assoc (rc-run-prep input-map)
          :transform-proj (capacity/supply-src-filter big-srcs filter-big?)
@@ -49,18 +63,22 @@
 ;;to do 1/4 of the reps on one machine, do rep-fraction= 1/4
 ;;to do those 1/4 of the reps two times, use (range 2) for the rep-indices.
 ;;file-tag is some descriptor for these set of runs to include in the filename
-;;threads is the number of cores on the machine that you are 
+;;threads is the number of cores on the machine that you are
 ;;running on.
 ;;rc-runs? is true or false depending on if you are doing the ra*rc runs or
 ;;just the ra runs.
 
 ;;This function will spit out one results.txt file for each fraction of reps.
+
+;;NOTE - this should be obviated with automatic run distribution and incremental
+;;patch.
 (defn variable-rep-runs [input-map rep-fraction rep-indices file-tag
                          threads rc-runs?]
+  (placeholder-error)
   (let [identifier (:identifier input-map)]
     (doseq [i rep-indices]
       (capacity/do-taa-runs (m4-path identifier)
-        (assoc (if rc-runs? (rc-run-prep input-map) input-map) 
+        (assoc (if rc-runs? (rc-run-prep input-map) input-map)
            ;;not used with :replicator
            ;;:reps num-reps
            :conj-proj {:replicator
