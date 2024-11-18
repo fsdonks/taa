1df93aaaebb75639a30b599ea3d5d3bbf04242ca
tom <tom@that-place>
2024-11-18 16:25:19 -0600

    bumped m4peer

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject taa "0.0.23-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [marathon "4.2.19-SNAPSHOT"]
-                 [m4peer "0.1.4-SNAPSHOT"]
+                 [marathon "4.2.20-SNAPSHOT"]
+                 [m4peer "7ff41c84f9606541f00afdb4502a3369f80d9a59" #_"0.1.4-SNAPSHOT"]
                  ;[com.clojure-goes-fast/clj-memory-meter "0.2.1"]
                  ;[techascent/tech.ml.dataset "7.000-beta-2"]
                  ]
